824b257cbc755dc4409b9ea553222a706f9e783a
tripleee <tripleee@users.noreply.github.com>
2019-03-09 09:11:38 +0200

    nst.py: read from stdin if sys.argv[1] isn't there

--- nst.py
+++ nst.py
@@ -11,8 +11,11 @@ import halflife
 logging.basicConfig(level=logging.WARN, format='%(module)s:%(message)s')
 with open('halflife.conf') as conffile:
     conf = json.loads(conffile.read())
-with open(sys.argv[1]) as jfile:
-    posts = json.loads(jfile.read())
+if len(sys.argv) == 2:
+    with open(sys.argv[1]) as jfile:
+        posts = json.loads(jfile.read())
+else:
+    posts = json.loads(sys.stdin.read())
 h = halflife.Halflife(conf['metasmoke-key'])
 for post in posts:
     try:
